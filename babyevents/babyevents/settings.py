a324b11ba1105699041e954bfe9b322009cd3386
Gus Mendez <gus.mendez.99@gmail.com>
2020-04-26 22:13:17 -0600

    preparing files to publish api in heroku

--- babyevents/babyevents/settings.py
+++ babyevents/babyevents/settings.py
@@ -15,7 +15,7 @@ import datetime
 
 # Heroku: Update database configuration from $DATABASE_URL. 
 import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500) 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,6 +93,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
